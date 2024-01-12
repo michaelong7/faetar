29440a3aa826d30dc4df20a96c7a41c0a61bcbf0
michaelong7 <michael.ong@mail.utoronto.ca>
2024-01-12 14:25:59 -0500

    replaced sort -V with sort

--- egs/faetar/local/hlvc_faetar_data_prep.sh
+++ egs/faetar/local/hlvc_faetar_data_prep.sh
@@ -28,7 +28,7 @@ function find_files () {
   for x in txt wav; do
     if [ ! -f "${x}list_${suffix}" ]; then
       find "$search_dir" -name "*.$x" |
-      sort -V |
+      sort |
       tee "${x}list_${suffix}" |
       tr '\n' '\0' |
       xargs -I{} -0 bash -c 'filename="$(basename "$1" '".$x"')"; echo ""$filename":"$1""' -- {} > "bn2${x}_${suffix}"
@@ -49,81 +49,49 @@ find_files "$test_dir" "test"
 find_files "$train_dir" "train"
 
 # merges bn2wav_train and bn2wav_test, and wavlist_traind and wavlist_test
-cat "bn2wav_test" "bn2wav_train" | sort -V > "bn2wav"
-cat "wavlist_test" "wavlist_train" | sort -V > "wavlist"
+cat "bn2wav_test" "bn2wav_train" | sort > "bn2wav"
+cat "wavlist_test" "wavlist_train" | sort > "wavlist"
 
-# # make symbolic links to wav files in links/ directory
-# mkdir -p links/
-# rm -f links/*
-# tr '\n' '\0' < wavlist |
-#   xargs -0 -I{} bash -c 'v="$(basename "$1" .wav)"; ln -sf "$1" "links/${v}.wav"' -- "{}"
+# make symbolic links to wav files in links/ directory
+mkdir -p links/
+rm -f links/*
+tr '\n' '\0' < wavlist |
+  xargs -0 -I{} bash -c 'v="$(basename "$1" .wav)"; ln -sf "$1" "links/${v}.wav"' -- "{}"
 
-# # now we can use Kaldi's table format
-# cat bn2wav | cut -d ':' -f 1 |
-#   awk -v d="$(cd links; pwd -P)" '{print $1, "sox "d"/"$1".wav -t wav -b 16 - rate 16k remix 1 |"}' > wav_unlab.scp
+# now we can use Kaldi's table format
+cat bn2wav | cut -d ':' -f 1 |
+  awk -v d="$(cd links; pwd -P)" '{print $1, "sox "d"/"$1".wav -t wav -b 16 - rate 16k remix 1 |"}' > wav_unlab.scp
 
-# # get those durations (lots of warnings - don't worry about those)
-# if [ ! -f "reco2dur_unlab" ]; then
-#   wav-to-duration "scp,s,o:wav_unlab.scp" "ark,t:reco2dur_unlab"
-# fi
-
-# # these mappings will be used to build collapsed partitions as well as a global 
-# # unlabelled partition (unlab)
-# cat reco2dur_unlab | \
-#   xargs -I{} bash -c 'read -ra v <<< "$1"; speaker=$(cut -d "_" -f 2 <<< "${v[0]}") ms=$(echo "${v[1]} * 100" | bc -l); printf "%s-%s-0000000-%06.0f %s 0.00 %.2f\n" "${v[0]}" "$speaker" "$ms" "${v[0]}" "${v[1]}"' -- {} \
-#   > segments_unlab
-# cut -d ' ' -f 1 segments_unlab > unlab.uttlist
-# cut -d ' ' -f 1 reco2dur_unlab > unlab.recolist
-# paste -d ' ' unlab.uttlist <(cut -d '-' -f 1-2 unlab.uttlist) > utt2spk_unlab
-
-# # construct kaldi files for train partition (labelled core to align with past scripts)
-
-# join -1 2 -2 1 <(sort segments_unlab | cut -d ' ' -f 1,2) <(tr ':' ' ' < bn2txt_train | sort) |
-# tr '\n' '\0' |
-# xargs -I{} -0 bash -c 'read -ra v <<< "$1"; text="$(cat ${v[2]})" ; printf "%s %s\n" "${v[1]}" "$text"' -- {} |
-# sort -V > "text_core"
-
-exit 20
-
-# convert eaf files to <utt_id> <rec_id> <spkr_id> <start_s> <end_s> <trans>
-cat bn2eaf | tr '\n' '\0' |
-  xargs -0 -I{} bash -c 'IFS=: read -ra v <<< "$1"; for i in $(seq 1 $((${#v[@]} - 1))); do '"$local"'/dump_eaf.py "${v[0]}" "${v[i]}" || true; done' -- "{}" |
-  sort -k 1,4 |
-  awk -f "$local/delete_nested_utterances.awk" |
-  awk '!(tolower($5) == "nn" || tolower($5) ~ /iver.*/ || $5 == "interviewer")' > eaf_dump_core
-
-# check that no utterances are going past the length of the file
-cut -d ' ' -f 2,4 eaf_dump_core |
-  awk 'BEGIN {id=""; last=""} $1 != id {if (last != "") print last} {id=$1; last=$0} END {print last}' |
-  join - reco2dur_unlab |
-  awk '$2 <= ($3 + 0.005) {print $1}' > keeplist
-if [ "$(cut -d ' ' -f 2 eaf_dump_core | uniq | wc -l)" != "$(cat keeplist | wc -l)" ]; then
-  echo "WARNING: some eaf files have longer segment lengths than recordings!"
-  diff <(cut -d ' ' -f 2 eaf_dump_core | uniq) keeplist || true
-  echo "Ignoring!"
+# get those durations (lots of warnings - don't worry about those)
+if [ ! -f "reco2dur_unlab" ]; then
+  wav-to-duration "scp,s,o:wav_unlab.scp" "ark,t:reco2dur_unlab"
 fi
 
-# construct kaldi files for core partition
-join -2 2 keeplist eaf_dump_core | 
-  cut -d ' ' -f 2,6- |
-  # Get rid of any segment containing '..'. It's how some segments mark
-  # "I didn't transcribe this". Can't put this in sanitize_text b/c it mucks
-  # with docx logic
-  grep -vF '..' |
-  perl -CS -n "$local/sanitize_text.pl" > text_core
-cut -d ' ' -f 1 text_core > core.uttlist
-awk -v "rlast=" '
-{
-  if (rlast != $2) {split("", spk2id); sidx=0}
-  if (spk2id[$5] == "") spk2id[$5] = ++sidx;
-  rlast=$2;
-  print $1, $2"-"spk2id[$5];
-}
-' eaf_dump_core | join core.uttlist - > utt2spk_core
+# these mappings will be used to build collapsed partitions as well as a global 
+# unlabelled partition (unlab)
+cat reco2dur_unlab | \
+  xargs -I{} bash -c 'read -ra v <<< "$1"; speaker=$(cut -d "_" -f 2 <<< "${v[0]}") ms=$(echo "${v[1]} * 100" | bc -l); printf "%s-%s-0000000-%06.0f %s 0.00 %.2f\n" "${v[0]}" "$speaker" "$ms" "${v[0]}" "${v[1]}"' -- {} \
+  > segments_unlab
+cut -d ' ' -f 1 segments_unlab > unlab.uttlist
+cut -d ' ' -f 1 reco2dur_unlab > unlab.recolist
+paste -d ' ' unlab.uttlist <(cut -d '-' -f 1-2 unlab.uttlist) > utt2spk_unlab
+
+# construct kaldi files for train partition
+
+join -1 2 -2 1 <(cut -d ' ' -f 1,2 < segments_unlab) <(tr ':' ' ' < bn2txt_train) |
+tr '\n' '\0' |
+xargs -I{} -0 bash -c 'read -ra v <<< "$1"; text="$(cat ${v[2]})" ; printf "%s %s\n" "${v[1]}" "$text"' -- {} > "text_train"
+cut -d ' ' -f 1 text_train > train.uttlist
+join utt2spk_unlab train.uttlist > utt2spk_train
+
+exit 20 
+
 cut -d ' ' -f 1-4 eaf_dump_core | join core.uttlist - > segments_core
 cut -d ' ' -f 2 segments_core | sort -u > core.recolist
 join core.recolist wav_unlab.scp > wav_core.scp
 join core.recolist reco2dur_unlab > reco2dur_core
+
+exit 20
 
 # kaldi likes utterances sorted by speaker id first. utterance ids have
 # DEADBEEF inserted in them, to be replaced by the speaker id
