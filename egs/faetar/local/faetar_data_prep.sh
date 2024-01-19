cff3228c653f693dc6ccf43f2b81a4a171d41443
michaelong7 <michael.ong@mail.utoronto.ca>
2024-01-19 15:39:41 -0500

    wav.scp only keeps files with length >= 500ms

--- egs/faetar/local/faetar_data_prep.sh
+++ egs/faetar/local/faetar_data_prep.sh
@@ -21,32 +21,34 @@ fi
 
 # construct kaldi files for given partition
 function construct_kaldi_files () {
-    search_dir="$1"
-    suffix="$2"
+  search_dir="$1"
+  suffix="$2"
 
-    # make wav.scp files
-    find "$search_dir" -name "*.wav" |
-    sort |
-    tr '\n' '\0' |
-    xargs -I{} -0 bash -c 'filename="$(basename "$1" '".wav"')"; echo -e ""$filename" "$1""' -- {} > "wav_${suffix}.scp"
+  # make wav.scp files
+  # only utterances with durations longer or equal to than 500 ms are retained
+  find "$search_dir" -name "*.wav" |
+  sort |
+  tr '\n' '\0' |
+  xargs -I{} -0 bash -c '
+    filename="$(basename "$1" '".wav"')"
+    dur="$(soxi -D "$1")"
+    if (( $(bc -l <<< "$dur >= 0.5") )); then
+      echo -e ""$filename" "$1""
+    fi' -- {} > "wav_${suffix}.scp"
 
-    # make text files
-    while read -r line; do
-        name="$(cut -d ' ' -f 1 <<< "$line")"
-        path="$(cut -d ' ' -f 2 <<< "$line")"
-        dur="$(soxi -D "$path")"
-        textfile="${path%%.wav}.txt"
-        text="$(< "$textfile")"
+  # make text files
+  while read -r line; do
+    name="$(cut -d ' ' -f 1 <<< "$line")"
+    path="$(cut -d ' ' -f 2 <<< "$line")"
+    dur="$(soxi -D "$path")"
+    textfile="${path%%.wav}.txt"
+    text="$(< "$textfile")"
 
-        # only utterances with durations longer or equal to than 500 ms are retained
-        if (( $(bc -l <<< "$dur >= 0.5") )); then
-            printf "%s-0000000-%06.0f %s\n" "$name" "$(bc -l <<< "$dur * 100")" "$text"
-        fi
-        
-    done < "wav_${suffix}.scp" > "text_${suffix}"
+    printf "%s-0000000-%06.0f %s\n" "$name" "$(bc -l <<< "$dur * 100")" "$text"
+  done < "wav_${suffix}.scp" > "text_${suffix}"
 
-    # make utt2spk files
-    awk 'BEGIN {FS = "_"} {print $0, $1}' <<< "$(cut -d ' ' -f 1 < "text_${suffix}")" > "utt2spk_${suffix}"
+  # make utt2spk files
+  awk 'BEGIN {FS = "_"} {print $0, $1}' <<< "$(cut -d ' ' -f 1 < "text_${suffix}")" > "utt2spk_${suffix}"
 
 }
 
@@ -63,6 +65,6 @@ construct_kaldi_files "$test_dir" "test"
 construct_kaldi_files "$train_dir" "train"
 
 # build LM
-cut -d ' ' -f 2- text_train | \
- "$local/ngram_lm.py" -o 1 --word-delim-expr " " | \
+cut -d ' ' -f 2- text_train |
+ "$local/ngram_lm.py" -o 1 --word-delim-expr " " |
  gzip -c > "lm.tri-noprune.gz"
