732165778cefe9b7b83536ff798db37231f99aa8
michaelong7 <michael.ong@mail.utoronto.ca>
2024-01-19 16:25:13 -0500

    readded segments file

--- egs/faetar/local/faetar_data_prep.sh
+++ egs/faetar/local/faetar_data_prep.sh
@@ -24,12 +24,13 @@ function construct_kaldi_files () {
   search_dir="$1"
   suffix="$2"
 
-  # make wav.scp files + text files
+  # make wav.scp files + text + segments files
   # only utterances with durations longer or equal to than 500 ms are retained
 
-  # clears "wav_${suffix}.scp" + "text_${suffix}" if they already exist / makes them if they don't
+  # clears files if they already exist / makes them if they don't
   :> "wav_${suffix}.scp"
   :> "text_${suffix}"
+  :> "segments_${suffix}"
 
   while read -r path; do
     name="$(basename "$path" .wav)"
@@ -39,7 +40,11 @@ function construct_kaldi_files () {
 
     if (( $(bc -l <<< "$dur >= 0.5") )); then
       echo ""$name" "$path"" >> "wav_${suffix}.scp"
-      printf "%s-000000-%06.0f %s\n" "$name" "$(bc -l <<< "$dur * 100")" "$text" >> "text_${suffix}"
+
+      printf "%s-000000-%06.0f %s\n" "$name" "$(bc -l <<< "$dur * 100")" "$text" |
+      tee -a "text_${suffix}" |
+      awk -v name="$name" -v dur="$dur" \
+      '{printf "%s %s 0.00 %.2f\n", $1, name, dur}' - >> "segments_${suffix}"
     fi
   done <<< "$(find "$search_dir" -name "*.wav" | sort)"
 
