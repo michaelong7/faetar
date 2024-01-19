5964bdf56919ac2cf19cd03937300e8c76b56c52
michaelong7 <michael.ong@mail.utoronto.ca>
2024-01-19 15:20:06 -0500

    renamed files + some edits

--- egs/faetar/local/faetar_format_data.sh
+++ egs/faetar/local/faetar_format_data.sh
@@ -17,10 +17,7 @@ for x in test train; do
   mkdir -p data/$x
   # rm -rf data/$x/* 
   cp -f data/local/data/wav_$x.scp data/$x/wav.scp
-  cp -f data/local/data/segments_$x data/$x/segments
   cp -f data/local/data/utt2spk_$x data/$x/utt2spk
-  cp -f data/local/data/reco2dur_$x data/$x/reco2dur
-  awk '{print $1, $1, "A"}' data/$x/reco2dur > data/$x/reco2file_and_channel
 
   utils/utt2spk_to_spk2utt.pl < data/$x/utt2spk > data/$x/spk2utt
 
