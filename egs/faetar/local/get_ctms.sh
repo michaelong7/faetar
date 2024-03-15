8542bdc8edca216c38c8fe1c8746a53a7a6bf4bb
michaelong7 <michael.ong@mail.utoronto.ca>
2024-03-15 18:16:05 +0000

    added function that adds silences properly

--- egs/faetar/local/get_ctms.sh
+++ egs/faetar/local/get_ctms.sh
@@ -1,8 +1,6 @@
 #!/usr/bin/env bash
 
-usage="Usage: $0 [-h] [--with-nooverlap {true,false}] [--clean {true,false}] [--frame-shift SEC] data-dir lang-dir exp-dir"
-clean=true
-with_nooverlap=false
+usage="Usage: $0 [-h] [--frame-shift SEC] data-dir lang-dir exp-dir"
 frame_shift=0.01
 help_message="Get CTM file from data and alignments and write to exp-dir/ctm
 
@@ -10,9 +8,6 @@ $usage
 e.g. data/core data/lang_rough exp/mono0
 
 --frame-shift     Seconds per frame increment (deft: $frame_shift)
---with-nooverlap  If true, generate exp-dir/ctm_nooverlap, which
-                  resolves overlapping word segments (deft: $with_nooverlap)
---clean           Clean temporary files (deft: $clean)
 "
 
 echo "$0 $@"  # Print the command line for logging
@@ -31,19 +26,69 @@ exp="$3"
 
 set -e
 
-steps/get_train_ctm.sh --frame-shift $frame_shift --print-silence true \
+# --print-silence in steps/get_train_ctm.sh gives incorrect values, so silence is added here
+function add_silence() {
+  ctm_file="$1"
+  awk \
+  'BEGIN {
+    FS = " ";
+    OFS = " ";
+    new_file = 0;
+    old_filename = "";
+    old_start = 0;
+    old_dur = 0;
+  }
+  
+  NR == FNR {
+    file_durs[$1] = $2;
+    next;
+  }
+  
+  {
+    filename = $1;
+    start = $3;
+    dur = $4;
+    
+    if (old_filename != filename) {
+      if (FNR != 1 && old_start + old_dur < file_durs[old_filename]) {
+        printf "%s A %.2f %.2f <eps>\n", old_filename, old_start + old_dur, file_durs[old_filename] - old_start - old_dur;
+      }
+      
+      if (start != 0) {
+        printf "%s A %.2f %.2f <eps>\n", filename, 0, start;
+        print $0;
+      }
+      else {
+        print $0;
+      }
+
+      old_filename = filename
+      old_start = start;
+      old_dur = dur;
+      next;
+    }
+
+    start_rounded = sprintf("%.2f", start);
+    old_end_rounded = sprintf("%.2f", old_start + old_dur);
+    if (start_rounded != old_end_rounded) {
+      printf "%s A %.2f %.2f <eps>\n", filename, old_start + old_dur, start - old_start - old_dur;
+      print $0;
+      old_start = start;
+      old_dur = dur;
+      next;
+    }
+    else {
+      print $0;
+      old_start = start;
+      old_dur = dur;
+      next;
+    }
+
+  }' "$data"/reco2dur "$ctm_file" > "$ctm_file"_
+  mv "$ctm_file"{_,}
+}
+
+steps/get_train_ctm.sh --frame-shift $frame_shift \
   "$data" "$lang" "$exp"
 
-if $with_nooverlap; then
-  tmp="$exp/ctm_segments"
-  mkdir -p "$tmp"
-  steps/get_train_ctm.sh --use-segments false --frame-shift $frame_shift \
-    "$data" "$lang" "$exp" "$tmp"
-  
-  utils/ctm/resolve_ctm_overlaps.py \
-    "$data/segments" "$tmp/ctm" - | \
-    utils/ctm/convert_ctm.pl "$data/"{segments,reco2file_and_channel} | \
-    sort -k1,1 -k2,2 -k3,3nb > "$exp/ctm_nooverlap"
-  
-  $clean && rm -rf "$tmp"
-fi
+add_silence "$exp"/ctm
